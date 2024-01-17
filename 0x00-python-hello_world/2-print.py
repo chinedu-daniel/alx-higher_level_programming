5d1cd9082c57d6b9e15f522aacf9d9191912793a
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-01-16 16:22:37 -0800

    2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 # 2-print.py
-def main():
-    print("Programming is like building a multilingual puzzle \n")
 
-main()
+print("Programming is like building multilingual puzzle\n")
