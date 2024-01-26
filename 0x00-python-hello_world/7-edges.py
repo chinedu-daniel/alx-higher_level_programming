987ebce2f59d0aa22d92a9ac7b332caddc8acd24
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-01-25 22:36:21 -0800

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = "Hol"
 word_last_2 = "on"
 middle_word = "olberto"
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"First 3 letters: {word_first_3}".format(word_first_3))
+print(f"Last 2 letters: {word_last_2}".format(word_last_2))
+print(f"Middle word: {middle_word}".format(middle_word))
