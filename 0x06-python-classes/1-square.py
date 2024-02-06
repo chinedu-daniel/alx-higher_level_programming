4b59455b904c948ea303eac717296b618bcdf288
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-06 05:52:50 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Define a Square class."""
 
 class Square:
+    """Represent a Square."""
+
     def __init__(self, size):
-        __size = None
+        """Initialize a Square.
+        
+        Args:
+            size (int): The size of the square
+        """
+
+        self.__size = size
