763662d6c730f175c88582eee543ebc0ce5a7be8
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-12 08:21:19 -0800

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+""" python 3 task """
 
 class Rectangle:
+
+    """
+
+    A class that defines a rectangle
+
+    """
+
     def __init__(self, width=0, height=0):
+
+        """ Instantiation of width and height """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+
+        """ width property """
+
         return self.__width
 
     @width.setter
@@ -19,6 +32,9 @@ class Rectangle:
 
     @property
     def height(self):
+
+        """height property"""
+
         return self.__height
 
     @height.setter
@@ -30,9 +46,15 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+
+        """public instance that returns the area of rectangle"""
+
         return self.__width * self.__height
 
     def perimeter(self):
+
+        """ Public instance that returns the perimeter of rectangle """
+
         if self.__width == 0 or self.__height == 0:
             return (0)
         return 2 * (self.__width + self.__height)
