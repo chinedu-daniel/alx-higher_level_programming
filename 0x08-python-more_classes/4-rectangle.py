9abfdeb3a6bae8afcb114bc5380ba0e5eb5ddf1a
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-12 08:43:58 -0800

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 
+""" python3 task """
+
 class Rectangle:
+
+    """
+
+    class that defines a rectangle
+
+    """
+
     def __init__(self, width=0, height=0):
+
+        """
+
+        instantiation of width af height
+
+        """
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
+
+        """ width property """
+
         return self.__width
 
     @width.setter
@@ -19,6 +38,9 @@ class Rectangle:
 
     @property
     def height(self):
+        
+        """ height property """
+
         return self.__width
 
     @height.setter
@@ -30,17 +52,28 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+
+        """ public instance that returns the area of a rectangle """
+
         return ((self.__width) * (self.height))
 
     def perimeter(self):
+
+        """ public instance that returns the perimeter of a rectangle """
+
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((2 * (self.__width) + (self.__height)))
 
     def __str__(self):
+
+        """ print the rectangle with the character """
+
         if self.__width == 0 or self.__height == 0:
             return ""
         return '\n'.join(['#' * self.__width] * self._height)
 
     def __repr__(self):
-        return f "Rectangle({self.__width}, {self.__height})"
+
+        """ returns  string representation of a rectangle """
+        return f"Rectangle({self.__width}, {self.__height})"
