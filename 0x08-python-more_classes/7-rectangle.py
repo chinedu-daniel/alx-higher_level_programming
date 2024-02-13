6f8d8be4c3f0ad17f2ddf62f255c9b724454d2f5
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-12 17:38:09 -0800

    7-rectangle.py

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+""" python3 task """
+
+class Rectangle:
+
+    """
+
+    class that defines a rectangle
+
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+
+        """
+
+        instantiation of with and height
+
+        """
+
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+
+        """ width property """
+
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+
+        """ height property """
+
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+
+        """ public instance that returns the area of the square """
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+
+        """ public instance that returns te perimeter of a rectangle """
+
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+
+        """ prints the rectangle with the characters stored """
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return ('\n'.join([str(self.print_symbol) * self.__width] * self.__height))
+
+    def __repr__(self):
+
+        """ returns a string representation of the rectangle """
+
+        return (f"Rectangle({self.__width}, {self.__height})")
+
+    def __del__(self):
+
+        """ prints the message with 3 dots """
+
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
