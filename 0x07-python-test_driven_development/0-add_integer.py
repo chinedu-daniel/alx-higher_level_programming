44673fea051deb7b1d5e8857191a0e7493f26285
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-15 08:53:20 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -25,14 +25,16 @@ def add_integer(a, b=98):
 
     # Check if a and b are integers or floats
 
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
 
     # Cast a and b to integers if they are floats
 
-    a = int(a)
-    b = int(b)
+    return int(a) + int(b)
 
-    # Return the addition of a and b
 
-    return (a + b)
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
