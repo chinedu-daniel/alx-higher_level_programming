4083492f4ecafe2955a4879a43e6600a2da988a4
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-20 06:12:06 -0800

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" python3 file """
+
+class BaseGeometry:
+    """
+
+    Base class for geometric shapes
+
+    This class provides common functionality and attributes for geometric shapes
+    Subclasses can inherit from this class to implement specific geometric shapes
+
+    Attributes:
+        None
+
+    Methods:
+        area(): Calculate the area of the geometric shape
+        perimeter(): Calculate the perimeter of the geometric shape
+
+    """
+
+    pass
