9e855ab01717bf38f40e58b3c19d93bfb25bb0e1
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-20 05:26:14 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" python3 file """
+
+def is_same_class(obj, a_class):
+
+    """
+
+    Check if the given object belongs to the specified class or any subclass
+
+    Parameters:
+        obj: object
+            The object to be checked.
+        a_class: typr
+            The class or type to compare against.
+
+    Returns:
+        bool:
+            True if the object is an instance of the specified class or any subclass,
+            False otherwise.
+
+    """
+
+    return type(obj) is a_class
