a1c31a207fa35406a77d0f2a39cde969fa9f4c37
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-21 09:55:11 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,17 @@ python3 file
 
 """
 
-def write_file(filename="", text=""):
-    with open(filename, 'w', encoding='UTF-8'):
 
+def write_file(filename="", text=""):
+    """
+
+    Writes the provided text to a file
+
+    Args:
+        filename(str, optiona): The name of the file to be written
+        text(str, optional): The text to be written to the file
+
+    """
+
+    with open(filename, 'w', encoding='UTF-8') as file:
+        return file.tell()
