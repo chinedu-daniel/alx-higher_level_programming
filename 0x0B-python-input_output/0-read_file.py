251dbf1daebe9df60302f447d6e8730447e3cd5a
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-21 09:25:07 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ python3 file """
 
+
 def read_file(filename=""):
+
     """
+
     Reads the content of a file and returns it as a string
 
     Args:
         filename (str, optional): The name of the file to be read
 
     """
-    
+
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+python3 file
+
+"""
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='UTF-8'):
+
