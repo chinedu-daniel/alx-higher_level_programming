aadd93bd4e43770357dc6e4a0971ded78ce31358
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-21 10:19:54 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" python3 file """
+
+
+def to_json_string(my_obj):
+    """
+
+    Convert the given python object to a JSON string
+
+    Args:
+        my_obj: The python object to be converted to JSON
+
+    Return:
+        str: A JSON string representing the input python object
+
+    """
+
+    return json.dumps(obj)
