bd95287ed0c72e1cbe14fe083c13d0750ab2e805
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-21 09:43:27 -0800

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,23 +2,24 @@
 
 """ python3 file """
 
+
 class BaseGeometry:
     """
     Base class for geometric shapes
 
-    This class provides common functionality and attributes for geometric shapes
-    Subclasses can inherit from this class to implement specific geometric shapes
+    This class provides common functionality and attributes
+    Subclasses can inherit from this class to implement specific
 
     """
 
     def area(self):
-        
+
         """
 
         Calculate the area of the geometric shape
 
         Raises:
-            Exception: indicates that the method is not implemented in the subclass
+            Exception: indicates that the method is not implemented
 
         """
 
