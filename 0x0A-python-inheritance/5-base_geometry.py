306b2cb2f90e9eb4fbe274374b1af56265a6464e
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-21 09:39:12 -0800

    5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ python3 file """
 
+
 class BaseGeometry:
     """
 
     Base class for geometric shapes
 
-    This class provides common functionality and attributes for geometric shapes
-    Subclasses can inherit from this class to implement specific geometric shapes
+    This class provides common functionality and attributes
+    Subclasses can inherit from this class to implement
 
     Attributes:
         None
