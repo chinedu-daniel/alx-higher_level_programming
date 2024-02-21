d99efcc46a3b2d0517a7e584ecc8227c65ccbbe2
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-21 12:53:41 -0800

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" python3 file """
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON formatted string into a python object
+
+    Parameters:
+    - my_str (str): A JSON formatted string to be converted
+
+    Return:
+    - obj: A python object representing the data encoded in JSON
+
+    Raises:
+    - ValueError: If the input string is not valid JSON
+
+    """
+
+    return json_loads(my_str)
