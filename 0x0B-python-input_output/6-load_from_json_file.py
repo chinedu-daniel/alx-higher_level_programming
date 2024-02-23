8403177280eca812c5e4faf05dc16a2246645a6b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-23 04:32:03 -0800

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" python3 file """
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Load a python object from a JSON file
+
+    Parameters:
+    - filename (str): THhe name of the JSON file to load from
+
+    Return:
+    - obj: A python object representing the stored data in JSON file
+
+    """
+
+    with open(filename, 'w', encoding='UTF-8') as file:
+        json.load(filename)
