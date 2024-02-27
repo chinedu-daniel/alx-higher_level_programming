b5883a30348be723f149aa62fd5d6ee1f8d82ee3
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-02-27 11:56:49 -0800

    corrected fourth commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,6 +81,3 @@ class Rectangle(Base):
     def display(self):
         for _ in range(self.height):
             print("#" * self.width)
-
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
