35a5984840ffe6a62d07032d7b3fc25b7070512e
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-03-28 18:47:50 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- Script: Creates the database and the user
-
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
-CREATE USER  IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'hbtn_0d_2';
+-- creates a user
+CREATE USER  IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grant SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
