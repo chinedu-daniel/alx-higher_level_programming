a3f5e710b2fc85d33fce7142a62ba003459ef54f
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-04-23 20:05:10 +0100

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const [, arg1, arg2] = process.argv;
+
+if (arg1 !== undefined && arg2 !== undefined) {
+  console.log(`${arg1} is ${arg2}`);
+} else {
+  console.log('Two arguments are required');
+}
