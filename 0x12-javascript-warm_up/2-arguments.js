4cf8083d5cb6ac99c288bb634d60c63858fbaffe
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-04-24 09:30:24 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.length;
+const argsLength = process.argv.length;
 
-if (args === 2) {
+if (argsLength === 2) {
   console.log('No argument');
-} else if (args === 3) {
+} else if (argsLength === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
