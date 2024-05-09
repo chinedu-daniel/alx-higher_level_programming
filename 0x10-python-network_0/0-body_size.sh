7b9799178880a968aded48264285cce419a72e4e
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-09 15:42:01 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # creates a URL and sends the request so it can be displayed
 
-curl -s I "$1" | grep -i  "Content-Length" | awk '{print $2}'
+curl -s I "$1" | grep  'Content-Length' | sed 's/^Content-Length: //'
