a71f330e3ce46bd934b1a037a211c5cc97d33676
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-10 17:15:15 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL, 
+
+sends a request to the URL,
+
+displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.request
+import sys
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
