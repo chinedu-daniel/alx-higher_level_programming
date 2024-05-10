632d8e5efc6970a98ba14b73f335aab644509aaa
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-10 16:02:34 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-# Python script that fetches a url
+"""Python script that fetches a url"""
 
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    htmml = response.read()
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
