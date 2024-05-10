5b2ada92981baceebcdd2c3926036085a5ee581c
Chinedu Daniel <danielpreciouschhinedu@gmail.com>
2024-05-10 17:59:24 +0100

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL 
+
+sends a request to the URL
+
+displays the value of the variable in the response header
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if 'X-Request-Id' in response.headers:
+        x_request_id = response.headers['X-Request-Id']
+        print(x_request_id)
+    else:
+        print("X-Request-Id not found in the response header.")
